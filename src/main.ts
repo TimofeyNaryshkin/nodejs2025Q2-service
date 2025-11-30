383c0a476584e3231dec72aa9f36d38f6b1b33e3
Timofey Naryshkin <naryshkint4@gmail.com>
2025-11-30 15:32:13 +0900

    feat: set server port to .env PORT

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import 'dotenv/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,8 @@ async function bootstrap() {
     whitelist: true,
     transform: true
   }))
-  await app.listen(4000);
+
+  const port = process.env.PORT || 4000;
+  await app.listen(port);
 }
 bootstrap();
