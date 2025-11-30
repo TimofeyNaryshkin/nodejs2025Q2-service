34b18ee7f71cf18310da8378329cfe6040d29e1b
Timofey Naryshkin <naryshkint4@gmail.com>
2025-11-30 23:12:16 +0900

    docs: change swagger docs title

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   );
 
   const config = new DocumentBuilder()
-    .setTitle('RSS Nodejs2025Q2-REST Service')
+    .setTitle('Home Library Service')
     .setDescription('Home music library service')
     .setVersion('1.0')
     .build();
